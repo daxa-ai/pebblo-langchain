5a1bf412ad9a3def05ef27862764a458dd71782f
Rajendra Kadam <raj.725@outlook.com>
2024-01-25 13:13:04 +0530

    Set default size to 0 instead of None

--- pebblo_langchain/langchain_community/document_loaders/pebblo.py
+++ pebblo_langchain/langchain_community/document_loaders/pebblo.py
@@ -226,7 +226,7 @@ class PebbloSafeLoader(BaseLoader):
         return file_owner_name
 
     def get_source_size(self, source_path: str) -> int:
-        size = None
+        size = 0
         if os.path.isfile(source_path):
             size = os.path.getsize(source_path)
         elif os.path.isdir(source_path):
